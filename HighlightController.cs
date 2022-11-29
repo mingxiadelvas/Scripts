d1801c7a245ae87120115c4503dd77b27005ef23
Delvas <ming_d21@hotmail.com>
2022-11-29 13:58:12 -0500

    Main Character

---
+++ HighlightController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightController : MonoBehaviour
+{
+    [SerializeField] GameObject highlighter;
+    GameObject currentTarget;
+    
+    public void Highlight(GameObject target){
+        if(currentTarget == target){
+            return;
+        }
+
+        currentTarget = target;
+        Vector3 position = target.transform.position + Vector3.up * 0.5f + Vector3.left * 0.18f;
+        Highlight(position);
+    }
+
+    public void Highlight(Vector3 position){
+        highlighter.SetActive(true);
+        highlighter.transform.position = position;
+    }
+
+    public void Hide(){
+        currentTarget = null;
+        highlighter.SetActive(false);
+    }
+}
---
+++ TalkInteract.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkInteract : Interactable
+{
+    public override void Interact(Character character){
+        Debug.Log("You talked with me congratulation");
+    }
+}
